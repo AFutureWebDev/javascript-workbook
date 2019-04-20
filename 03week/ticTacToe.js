d1d75fdab0517ceadfc058c9bacb6d357881ce3f
AFutureWebDev <jenna.gullick2@gmail.com>
2019-04-20 12:24:57 -0500

    finished ttt

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -1,11 +1,6 @@
 'use strict';
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
@@ -23,28 +18,23 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
-function resetBoard() {
-  console.log("clear")
-  let boxes = document.getElementsByClassName("box");
-  for (var i=0; i<boxes.length; i++) {
-      boxes[i].innerHTML = "";
-  }
-}
+// function resetBoard() {
+//   console.log("clear")
+//   board = document.getElementsByClassName("box");
+//   for (var i=0; i<board.length; i++) {
+//       board[i].innerHTML = "";
+//   }
+// }
 
 function horizontalWin() {
   // Possible horizontal wins
-  let winArrH = board[0][0];
   if ((board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn) || (board[1][0] == playerTurn && board[1][1] == playerTurn && board[1][2] == playerTurn) || (board[2][0] == playerTurn && board[2][1] == playerTurn && board[2][2] == playerTurn)) {
     return true;
   }
-  document.getElementById(winner).innerHTML(playerTurn + 'Wins!');
 }
-
-horizontalWin();
 
 function verticalWin() {
   // Possible vertical wins
-  let winArrV = board[0][0];
   if ((board[0][0] == playerTurn && board[1][0] == playerTurn && board[2][0] == playerTurn) || (board[0][1] == playerTurn && board[1][1] == playerTurn && board[2][1] == playerTurn) || (board[0][2] == playerTurn && board[1][2] == playerTurn && board[2][2] == playerTurn)) {
     return true;
   }
@@ -52,7 +42,6 @@ function verticalWin() {
 
 function diagonalWin() {
   // Possible diagonal wins
-  let winArrD = board[0][0];
   if ((board[0][0] == playerTurn && board[1][1] == playerTurn && board[2][2] == playerTurn) || (board[0][2] == playerTurn && board[1][1] == playerTurn && board[2][0] == playerTurn)) {
     return true;
   }
@@ -60,42 +49,28 @@ function diagonalWin() {
 
 function checkForWin() {
   // check all the possible win options
-  if (horizontalWin()) {                  // check if there is a horizontal win
-    return true;
-  } else if (verticalWin()) {             // check if there is a vertical win
-    return true;
-  } else if (diagonalWin()) {             // check if there is a diagonal win
-    return true;
+  if (horizontalWin() || verticalWin() || diagonalWin()) {   
+          // If there is a win, display on the webpage...
+    document.getElementById("winner").innerText = (playerTurn + ' Wins!');
   }
 }
 
-function ticTacToe(row, column) {
+function ticTacToe(row, column, id) {
+  if(board[row][column] == " ") {
+  document.getElementById(id).innerHTML = (playerTurn);
+  board[row][column] = playerTurn;
+  console.log(board);
+  checkForWin();
   // If player X makes a move, return an X in the designated position, then switch to player O
   if (playerTurn === "X") {
-    board[row][column] = playerTurn;
     playerTurn = "O";
 
   // If player 0 makes a move, return an O in the designated position, then switch to player X
   } else if (playerTurn === "O") {
-    board[row][column] = playerTurn;
     playerTurn = "X";
-  } 
+    }
+  }
 }
-
-
-function getPrompt() {
-  printBoard();
-  console.log("It's Player " + playerTurn + "'s turn.");
-  rl.question('row: ', (row) => {
-    rl.question('column: ', (column) => {
-      ticTacToe(row, column);
-      getPrompt();
-    });
-  });
-
-}
-
-
 
 // Tests
 
